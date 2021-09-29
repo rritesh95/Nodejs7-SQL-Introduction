afa6eb645370e477e300f0e5b8f81fb155f9f437
Ritesh Patel <rritesh95@gmail.com>
2021-09-29 19:30:30 +0530

    uploade my full code

---
+++ app.js
@@ -0,0 +1,26 @@
+//functionality without MySQL query may fail while working with products
+//as currently we have products table only
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+app.listen(3000);
